a3c021ceba0f99deae523e42c283c5e02ec3ac3a
Jkimishere <106307588+Jkimishere@users.noreply.github.com>
2022-10-27 22:59:11 +0900

    changed array to arr

--- torchutils.basic.py
+++ torchutils.basic.py
@@ -3,9 +3,9 @@ import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def train_test_split(array, train):
+def train_test_split(arr, train):
     if train:
-        array = array[0:int(len(array) * 0.9)] #split images(first 90%)
+        arr = arr[0:int(len(arr) * 0.9)] #split images(first 90%)
     else:
-        array = array[int(len(array) * 0.9):] #split images(last 10%)
+        arr = arr[int(len(arr) * 0.9):] #split images(last 10%)
     
