51c713fdd5ea552c335ca94a0b30d107e1644c34
Jkimishere <106307588+Jkimishere@users.noreply.github.com>
2024-02-19 15:13:25 +0900

    Make code a little bit better

--- torchutils_loops.py
+++ torchutils_loops.py
@@ -1,94 +1,52 @@
-#pytorch imports
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch.utils.data as data
-from torchvision import transforms, datasets
-#other imports
-import os
-from PIL import Image
-from time import time
-
-
-
-#training loop
-def training_loop(model, epochs, trainloader, loss_fn, optimizer, scheduler, plot_graph : bool) -> None:
-        print('training start')
-        print(len(trainloader))
-        tl = []
+def training_loop(model, epochs, train_loader, val_loader, loss_fn, optimizer, model_name, scheduler=None, plot_graph : bool =True) -> None:
+        print(f'starting training for model {model_name}')
+        training_losses = []
+        validation_losses = []
         training_start = time()
         for epoch in range(epochs):
             print(f'epoch {epoch}')
             loss = 0.0
-            start = time()
+            epoch_start = time()
             model.train() 
-            for i, data in enumerate(tqdm(trainloader, leave=False,position=1)):
-                img, label = data
-                out = model(img)
-                loss = loss_fn(out, label)
-                if plot_graph:
-                    tl.append(loss.cpu().item())
+            for i, data in enumerate(tqdm(train_loader, leave=False,position=1)):
+                x, y = data
+                out = model(x)
+                loss = loss_fn(out, y)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
             if scheduler:
                 scheduler.step()
-            end = time()
-            print(f'epoch {epoch} ended with loss {loss} ||| epoch {epoch} runtime : {end - start} seconds')
+            epoch_end = time()
+            if plot_graph:
+                training_losses.append(loss.cpu().item())
+            print(f'epoch {epoch} ended with loss {loss} ||| epoch {epoch} runtime : {epoch_end - epoch_start} seconds')
             print('starting validation')
-            validation_loop()
+            validation_losses.append(validation_loop(model, val_loader))
         training_end = time()
         print(f'training done in {int(training_end - training_start)} seconds, or {int(training_end - training_start) / 60} minutes')
-        torch.save(model.state_dict(), './Model.pth')
+        torch.save(model.state_dict(), f'./{model_name}.pth')
         if plot_graph:
-            plt.figure(figsize=(10,5))
-            plt.title("Training Loss")
-            plt.plot(tl,label="train")
-            plt.plot(vl, label='validation loss')
-            plt.plot(va, label = 'validation f1 score')
-            plt.xlabel("iterations")
-            plt.ylabel("Loss")
-            plt.show()
+            plt.plot(list(range(epochs)), training_losses, label='Training loss')
+            plt.plot(list(range(epochs)), validation_losses, label='Validation loss')
+            plt.title('Loss graph')
             plt.legend()
-
-
-#testing loop
-def testing_loop(model,testloader):
-        model.load_state_dict(torch.load('./Model.pth'))
-        correct = 0
-        total = 0
-        model.eval()
-        # since we're not training, we don't need to calculate the gradients for our outputs
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data
-                # calculate outputs by running images through the network
-                outputs = model(images)
-                # the class with the highest energy is what we choose as prediction
-                _, predicted = torch.max(outputs.data, 1)
-                total += 1
-                correct += (predicted == labels).sum().item()
-                
-                
-                
-vl = []
-va = []
-vf1 = []       
-
-def validation_loop(model, valloader,loss_fn, is_loss, is_accuracy, is_f1):
-    torch.inference_mode()
-    for i, data in enumerate(valloader):
-        img, label = data
-        out = model(img)
-        pred = out.cpu().detach().numpy()
-        target = label.cpu().detach().numpy()
-        if is_loss:
-            #use loss function
-            loss = loss_fn(out, label)
-            vl.append(loss.cpu().item())
-        if is_accuracy:
-            #use accuracy
-            va.append(accuracy_score(target,pred))
-        if is_f1:
-            vf1.append(f1_score(target,pred))
+            plt.show()
+        
+        
+def validation_loop(model, val_loader):
+    model.eval()
+    total_loss = 0.0
+    correct_preds = 0
+    total_preds = 0
+    with torch.no_grad():
+        for x, y in val_loader:
+            x, y = inputs.to(device), labels.to(device)
+            
+            outputs = model(x)
+            loss = criterion(outputs, y)
+            
+            total_loss += loss.cpu().item() * x.size(0)
+    avg_loss = total_loss / len(val_loader.dataset)
+    print(f'Average loss for model in validation is : {avg_loss}')
+    return avg_loss
